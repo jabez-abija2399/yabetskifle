17ef4d08c77c26aaa910cd2bb1aee13c87d18975
jabez-abija2399 <jabeznew2399@gmail.com>
2026-04-22 16:51:29 +0300

    feat: add featured boolean to project type and filter featured projects in hook and wrapper

--- src/components/sections/ProjectsWrapper.tsx
+++ src/components/sections/ProjectsWrapper.tsx
@@ -1,11 +1,11 @@
 "use client" 
 
 import { useProjects } from "@/hooks/useProjects"
-import { Projects } from "./Projects"
+import { Projects,  } from "./Projects"
 
 export const ProjectsWrapper = () => {
   // All data logic is here, away from page.tsx
-  const { projects, isLoading } = useProjects()
+  const { projects, isLoading, featuredProjects } = useProjects()
 
-  return <Projects projects={projects} isLoading={isLoading} />
+  return <Projects  projects={featuredProjects}  isLoading={isLoading} />
 }
