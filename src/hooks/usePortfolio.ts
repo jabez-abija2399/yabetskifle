d690e5f3462303b87e94ad5ce548a0e1153a9ed1
jabez-abija2399 <jabeznew2399@gmail.com>
2026-04-22 13:50:56 +0300

    feat: integrate Supabase to fetch and update portfolio hero data dynamically

--- src/hooks/usePortfolio.ts
+++ src/hooks/usePortfolio.ts
@@ -1,25 +1,62 @@
-import { useState } from "react";
+import { createSupabaseClient } from "@/lib/supabase";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 
 interface HeroData {
-    name: string;
-    role: string;
-    bio: string;
+  name: string;
+  role: string;
+  bio: string;
 }
 
 
 export const usePortfolio = () => {
-    const [heroData, setHeroData] = useState<HeroData>({
-    name: "Yabets Kifle",
-    role: "Frontend Developer",
-    bio: "I am a frontend developer with a passion for building beautiful and user-friendly web applications..."
+  const [heroData, setHeroData] = useState<HeroData>({
+    name: "Loading...",
+    role: "Loading...",
+    bio: "Loading..."
   })
 
   const [isLoading, setIsLoading] = useState(false);
+  const supabase = createSupabaseClient();
 
-   const updateHero = (newData: HeroData) => {
-    setHeroData(newData)
-    console.log("Saving to database...", newData)
+  useEffect(() => {
+    const fetchSetting = async () => {
+      const {data, error} = await supabase
+      .from('portfolio_settings')
+      .select('*')
+      .limit(1)
+      .single();
+
+      if (data) {
+        setHeroData({
+          name: data.name,
+          role: data.role,
+          bio: data.bio
+        });
+      }
+
+      setIsLoading(false);
+    }
+
+    fetchSetting();
+  }, [])
+
+  const updateHero = async (newData: HeroData) => {
+    setIsLoading(true);
+
+    const {error} = await supabase
+    .from('portfolio_settings')
+    .update(newData)
+    .eq('name', heroData.name)
+
+    if (error) {
+       toast.error("Failed to save" + error.message);
+    }else {
+      setHeroData(newData);
+      toast.success("Saved successfully");
+    }
+    setIsLoading(false);
   }
 
 
